dc126530b6a1cef393f7ae49181afa628e5e0d2d
Caetano Burjack <caetano.burjack@gmail.com>
2022-11-25 20:22:29 +0000

    Status stylization

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,63 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
 export function History() {
   return (
-    <>
-      <h1>History</h1>
-    </>
+    <HistoryContainer>
+      <h1>My History</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Time</th>
+              <th>Started at</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Task</td>
+              <td>25 min</td>
+              <td>Leitura do livro X</td>
+              <td>
+                <Status status="warning" />
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>25 min</td>
+              <td>Leitura do livro X</td>
+              <td>
+                <Status status="warning" />
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>25 min</td>
+              <td>Leitura do livro X</td>
+              <td>
+                <Status status="warning" />
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>25 min</td>
+              <td>Leitura do livro X</td>
+              <td>
+                <Status status="warning" />
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>25 min</td>
+              <td>Leitura do livro X</td>
+              <td>
+                <Status status="warning" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   );
 }
